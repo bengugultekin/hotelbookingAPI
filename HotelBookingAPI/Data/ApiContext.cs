0624fe05060df462462e7e235209c1873a3cb40a
bengugultekin <bengunurgultekin54@gmail.com>
2023-07-09 10:17:10 +0300

    crated get, post, delete, getall api endpoints

---
+++ HotelBookingAPI/Data/ApiContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HotelBookingAPI.Models;
+
+namespace HotelBookingAPI.Data
+{
+    public class ApiContext : DbContext
+    {
+
+        public DbSet<HotelBooking> Bookings { get; set; }
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) 
+        {
+            
+        }
+    }
+}
